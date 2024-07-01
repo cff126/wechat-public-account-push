f54483f0624cf5ae9c166e125073582b31fdc81f
cff126 <chenfangfang@my.swjtu.edu.cn>
2024-07-01 14:42:58 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -98,7 +98,7 @@ const USER_CONFIG = {
       id: 'oENvb6LLc1-FRZrEHWcHe3YY1id4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       // 使用其他通道时，请填写 config/template-config.cjs 中某个想要使用的模板的id
-      useTemplateId: '45ZRw3SI_CeE8bV9_zCstfD6X0yA5NaORas-5QAxIP4',
+      useTemplateId: '5qcN88qUcIpc8JgdWXNsQIcAu5yCYqzlPaYnkonHAyE',
       // 所在省份或城市，也可以不填
       province: '四川',
       // 所在城市或县区
@@ -224,7 +224,7 @@ const USER_CONFIG = {
 
   // 使用微信测试号：【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
   // 使用其他通道时，请填写 config/template-config.cjs 中【推送完成提醒】模板的id
-  CALLBACK_TEMPLATE_ID: '45ZRw3SI_CeE8bV9_zCstfD6X0yA5NaORas-5QAxIP4',
+  CALLBACK_TEMPLATE_ID: '5qcN88qUcIpc8JgdWXNsQIcAu5yCYqzlPaYnkonHAyE',
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
