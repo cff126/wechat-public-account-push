73fadb6ac12d5b75ae25f9715b3d2ab7fe5bc8aa
cff126 <chenfangfang@my.swjtu.edu.cn>
2024-07-01 13:44:52 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -45,7 +45,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oENvb6LLc1-FRZrEHWcHe3YY1id4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '3mSLqBcpc0jqEF-mVc7AnTUA4ZbXKOt8Mr5--h27e9w',
+      useTemplateId: 'sZ1NTzFGl-bSUsihvpje3mBvsESyR-HZGRUCLZxA0t4',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '1-26',
       festivals: [
@@ -73,7 +73,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '3mSLqBcpc0jqEF-mVc7AnTUA4ZbXKOt8Mr5--h27e9w',
+  CALLBACK_TEMPLATE_ID: 'sZ1NTzFGl-bSUsihvpje3mBvsESyR-HZGRUCLZxA0t4',
 
   CALLBACK_USERS: [
     {
