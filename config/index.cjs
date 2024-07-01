dd04669deaf1748c0a65f0ce600cf17a8e56bab9
cff126 <chenfangfang@my.swjtu.edu.cn>
2024-07-01 12:49:27 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -56,7 +56,7 @@ const USER_CONFIG = {
 
    TIAN_API: {
     // 天行API KEY，如果使用天行API则需要填写此项
-    key: '',
+    key: '7f463458b944b9abe483cc26c835db63',
     
     /** 天行API相关，需要config中配置 TIAN_API_KEY  */
     // 早安心语, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
